3390d08678e71fc83919b7547f2e887e36ccd4f8
Ibn Ahmad Bello <justforlearningcode@gmail.com>
2017-12-16 15:24:17 +0100

    ViewPager

--- app/src/main/java/com/regent/tech/ramadanguide/MainActivity.java
+++ app/src/main/java/com/regent/tech/ramadanguide/MainActivity.java
@@ -1,13 +1,20 @@
 package com.regent.tech.ramadanguide;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Cast the ViewPager and find it from the activity_main
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+
     }
 }
