d06542caf664ad0193562c2515d5c71dec683e80
Ibn Ahmad Bello <justforlearningcode@gmail.com>
2017-12-16 15:41:21 +0100

    Created FragmentAdapter.java

---
+++ app/src/main/java/com/regent/tech/ramadanguide/FragmentAdapter.java
@@ -0,0 +1,12 @@
+package com.regent.tech.ramadanguide;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by root on 12/16/17.
+ */
+
+public class FragmentAdapter extends FragmentPagerAdapter {
+    public FragmentPagerAdapter(FragmentManager fm){super(fm);}
+}
