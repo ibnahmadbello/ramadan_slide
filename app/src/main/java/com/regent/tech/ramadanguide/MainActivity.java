23e9df16f4b8d63b770d8abc10f742747ca7606b
Ibn Ahmad Bello <justforlearningcode@gmail.com>
2018-01-22 22:06:02 +0100

    RadioButton responded to touch to display appropriate slide/page

--- app/src/main/java/com/regent/tech/ramadanguide/MainActivity.java
+++ app/src/main/java/com/regent/tech/ramadanguide/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.annotation.IdRes;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
